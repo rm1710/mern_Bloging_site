7da538961cece4540156fd814ae4341d302801a7
rm1710 <d290mishraritesh@gmail.com>
2024-11-21 23:04:57 +0530

    sync

--- backend/src/routes/blog.route.js
+++ backend/src/routes/blog.route.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',a(req,res)=>{});
+router.get('/',async(req,res)=>{
+    res.send("Blog routes is here")
+});
 module.exports = Blog;
